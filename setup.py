1e290a4bc3b69cc9c48f15ec58c000e96bd59fbc
= <=>
2023-02-28 15:57:47 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name="nlp_project", version="0.0.1", author="Oumar Kane", author_email="oumar.kane@univ-thies.sn",
+      description="Make fast text processing with a text processing pipeline. Contains utils and useful classes with tokenizer with many options, corpus extraction, tokens and n-grams frequency processing and visualization, etc. The processes can be recuperated as a pipeline for a different corpus.")
