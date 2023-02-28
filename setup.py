50abb6aefeae027fd2c077a6e273e21087184718
= <=>
2023-02-28 16:04:09 +0000

    setup updating

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
 
-setup(name="nlp_project", version="0.0.1", author="Oumar Kane", author_email="oumar.kane@univ-thies.sn",
-      description="Make fast text processing with a text processing pipeline. Contains utils and useful classes with tokenizer with many options, corpus extraction, tokens and n-grams frequency processing and visualization, etc. The processes can be recuperated as a pipeline for a different corpus.")
+setup(name="nlp_project", version="0.0.1", author="Oumar Kane", author_email="oumar.kane@univ-thies.sn", 
+      description="Make fast text processing with a text processing pipeline. Contains utils and useful classes with tokenizer with many options, corpus extraction, tokens and n-grams frequency processing and visualization, etc. The processes can be recuperated as a pipeline for a different corpus.",
+      requires=['spacy', 'nltk', 'gensim'])
